72fc0c4b0cadfd8856b056e29c7db86b4aba7ee3
Hadrien de Caumont <hadrien.decaumont@gmail.com>
2017-02-13 17:19:01 +0100

    erreur de schlag master "

---
+++ app/controllers/camps_controller.rb
@@ -0,0 +1,48 @@
+class CampsController < ApplicationController
+before_action :set_camp, only: [:show, :edit, :update, :destroy]
+  def index
+    @camps = Camp.all
+  end
+
+  def show
+  end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.new(camp_params)
+    if @camp.save
+      redirect_to camp_path(@camp)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @camp.update(camp_params)
+    if @camp.save
+      redirect_to camp_path(@camp)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def camp_params
+    params.require(:camp).permit(:title, :description, :address, :price)
+  end
+
+  def set_camp
+    @camp = Camp.find(params[:id])
+  end
+
+end
