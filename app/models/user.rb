ac2174dd4b564d890acc4dad1a274d97896371ab
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-14 13:07:57 +0100

    setup fb connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
   #associations
   has_many :camps, dependent: :destroy
   has_many :bookings, dependent: :destroy
