b94d081a18f605dd11de6f9f4588d8e2819e86d1
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-14 18:21:23 +0100

    really minor change

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,7 +18,7 @@ class BookingsController < ApplicationController
     if @booking.save
         redirect_to bookings_path
       else
-        render 'pages/camp'
+        render 'camps/show'
     end
   end
 
