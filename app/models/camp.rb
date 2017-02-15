59daa11bdf6a3519093f915fd71ee7a285240fc9
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-02-15 10:42:04 +0100

    migration lont lag to camps

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -9,4 +9,7 @@ class Camp < ApplicationRecord
   validates :price, presence: true
 
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
